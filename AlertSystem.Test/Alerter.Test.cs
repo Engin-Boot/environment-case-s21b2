fa25c61269d65ef235149b30695e718529c70f47
Tripti Saini <triptisaini123@gmail.com>
2020-09-17 13:12:05 +0530

    Add ReportToCsvFile type alerter and test cases

---
+++ AlertSystem.Test/Alerter.Test.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace AlertSystem.Test
+{
+    public class Alerter
+    {
+        private readonly IAlerter _csvAlerter;
+
+        public Alerter()
+        {
+            _csvAlerter = new AlertByReportToCsvFile("EnvironmentIssuesReport.csv");
+        }
+
+        [Fact]
+        public void TestSendAlert()
+        {
+            _csvAlerter.SendAlert("Humidity", ParameterStatus.VeryHigh, BreachLevel.Error);
+            _csvAlerter.SendAlert("Temperature", ParameterStatus.Low, BreachLevel.Warning);
+        }
+        
+    }
+}
---
+++ AlertSystem/IAlerter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using CsvFileReaderAndWriter;
+using Microsoft.VisualBasic;
+
+namespace AlertSystem
+{
+    public interface IAlerter
+    {
+        public void SendAlert(string parameter, ParameterStatus status, BreachLevel level);
+    }
+
+    public class AlertByReportToCsvFile: IAlerter
+    {
+        private readonly CsvFileWriter _writer;
+
+        public AlertByReportToCsvFile(string fileName)
+        {
+            _writer = new CsvFileWriter(fileName);
+            List<string> columns = new List<string>() { "Date and Time", "Parameter", "Parameter Status", "Breach Level/ Alert Type" };
+            _writer.WriteRow(columns);
+
+        }
+
+        ~AlertByReportToCsvFile()
+        {
+            _writer.Dispose();
+        }
+
+        public void SendAlert(string parameter, ParameterStatus status, BreachLevel level)
+        {
+            
+            List<string> columns = new List<string>()
+            {
+                DateAndTime.Now.ToString(CultureInfo.InvariantCulture), 
+                parameter, 
+                status.ToString(), 
+                level.ToString()
+            };
+            _writer.WriteRow(columns);
+        }
+    }
+}
