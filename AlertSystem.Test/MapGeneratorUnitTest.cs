fb08f9e2532b8675cdc39d23428cddbe72be8ba3
Tripti Saini <triptisaini123@gmail.com>
2020-09-21 17:57:13 +0530

    Refactor code

---
+++ AlertSystem.Test/MapGeneratorUnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using Xunit;
+
+namespace AlertSystem.Test
+{
+    public class MapGeneratorUnitTest
+    {
+        [Fact]
+        public void CheckParameterMapsAreNotNull()
+        {
+            Assert.NotNull(MapGenerator.TemperatureRangeMap);
+            Assert.NotNull(MapGenerator.HumidityRangeMap);
+        }
+    }
+}
