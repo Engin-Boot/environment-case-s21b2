d612d4d5e66478f9f40499f1c48d1aa94369a9e5
Tripti Saini <triptisaini123@gmail.com>
2020-09-22 22:02:01 +0530

    Remove duplicate code

--- CsvFileReaderAndWriter.Test/CsvFileReaderUnitTest.cs
+++ CsvFileReaderAndWriter.Test/CsvFileReaderUnitTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using MonitoringSensor;
 using Xunit;
@@ -10,18 +11,20 @@ namespace CsvFileReaderAndWriter.Test
 {
     public class CsvFileReaderUnitTest
     {
+        private bool IsReadRowPossibleForGivenInput(string testInput, List<string> columns)
+        {
+            Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(testInput));
+            using var reader = new CsvReader(stream);
+            var result = reader.ReadRow(columns);
 
+            return result;
+        }
         [Fact]
         public void WhenValidColumnsListIsEmptyThenReturnFalseToShowEndOfFile()
         {
             var testInput = string.Empty;
             var columns = new List<string>();
-            bool result;
-            Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(testInput));
-            using (var reader = new CsvReader(stream))
-            {
-                result = reader.ReadRow(columns);
-            }
+            bool result = IsReadRowPossibleForGivenInput(testInput, columns);
             Debug.Assert(columns.Count == 0);
             Assert.False(result);
         }
@@ -32,12 +35,7 @@ namespace CsvFileReaderAndWriter.Test
 
             const string testInput = "123,456,789,0";
             var columns = new List<string>();
-            bool result;
-            Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(testInput));
-            using (var reader = new CsvReader(stream))
-            {
-                result = reader.ReadRow(columns);
-            }
+            bool result = IsReadRowPossibleForGivenInput(testInput, columns);
             Assert.True(result);
             Debug.Assert(columns.Count == 4);
             Debug.Assert(columns[0] == "123");
